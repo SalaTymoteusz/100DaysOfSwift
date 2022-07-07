5ae437f77c63286d22cbae725c751ad36382e082
iOS Developer <tymek.pawel@gmail.com>
2022-07-07 19:14:13 +0200

    multi-line strings

--- day_1.playground/Contents.swift
+++ day_1.playground/Contents.swift
@@ -12,3 +12,25 @@ var population = 8_000_000
 
 // Optional
 var meaningOfLife = 42
+
+
+// MARK: MULTI-LINE STRINGS
+/*
+ The opening and closing triple must be on THEIR OWN LINE, but opening and
+ closing line breaks won’t be included in your final string.
+*/
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+/*
+ If you only want multi-line strings to format your code neatly, and you don’t
+ want those line breaks to actually be in your string, end each line with a \
+*/
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
