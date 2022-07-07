69d53749b14f13b662d3e4f4a1799aeb3d2ceac4
iOS Developer <tymek.pawel@gmail.com>
2022-07-07 18:47:35 +0200

    strings and integers

--- day_1.playground/Contents.swift
+++ day_1.playground/Contents.swift
@@ -4,3 +4,11 @@ import UIKit
 var favoriteShow = "Orange is the New Black"
 favoriteShow = "The Good Place"
 favoriteShow = "Doctor Who"
+
+
+// MARK: STRINGS AND INTIGERS
+var age = 38
+var population = 8_000_000
+
+// Optional
+var meaningOfLife = 42
